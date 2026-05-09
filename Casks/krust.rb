2d1cd846d410c7fb31589b992ca4ca60b32ad0e5
vanchonlee <vanchonlee@gmail.com>
2026-05-10 00:32:17 +0700

    Release krust v1.4.2

--- Casks/krust.rb
+++ Casks/krust.rb
@@ -1,6 +1,6 @@
 cask "krust" do
-  version "1.4.1"
-  sha256 "12cd8344164476371847a2324eccc15c2829c4f27101967df3ba2ab9d0de69fc"
+  version "1.4.2"
+  sha256 "338a6ff610a0dc02b6a40595c8da43e05a3dd970ab055f5920bac93c1e9c70b6"
 
   url "https://github.com/vanchonlee/homebrew-tap/releases/download/v#{version}/krust-#{version}.dmg",
       verified: "github.com/vanchonlee/homebrew-tap/"
@@ -11,6 +11,7 @@ cask "krust" do
   depends_on macos: ">= :sonoma"
 
   app "krust.app"
+  binary "#{appdir}/krust.app/Contents/MacOS/k9r", target: "k9r"
 
   zap trash: [
     "~/Library/Caches/chonle.krust",
